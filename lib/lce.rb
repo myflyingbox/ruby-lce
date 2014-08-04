bab75b1d9504c461550342fb9e7b8517cb68c2ca
Florent <florent.piteau@sylogix.net>
2014-08-04 18:29:53 +0200

    Handle configuration

--- lib/lce.rb
+++ lib/lce.rb
@@ -1,6 +1,29 @@
 require "lce/version"
 
 module Lce
-    # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :login, :password, :environment
+
+    def initialize
+      @environment = :staging
+    end
+    
+    def environment=(value)
+      raise 'Environment must be :staging or :production' unless [:staging, :production].include?(value)
+      @environment = value
+    end
+  end
 end
 
--- spec/lce_spec.rb
+++ spec/lce_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper'
 
 describe Lce do
+  describe "#configure" do
+
+    it 'has a configuration'  do
+      expect(Lce.configuration).to be_a(Lce::Configuration)
+    end
+   
+    it 'has a default environment'  do
+      expect(Lce.configuration.environment).to be :staging
+    end
+
+    it 'raises an exception when an environment is not supported'  do
+      expect { Lce.configuration.environment = :jungle }.to raise_error
+    end
+
+    it 'doesn\'t have a default login'  do
+      expect(Lce.configuration.login).to be_nil
+    end
+
+    it 'doesn\'t have a default password'  do
+      expect(Lce.configuration.password).to be_nil
+    end
+
+    context 'when configuration is overridden locally' do
+      before do
+        Lce.configure do |config|
+          config.environment = :production
+          config.login = 'login'
+          config.password = 'password'
+        end         
+      end
+      
+      it 'can override default values'  do
+        expect(Lce.configuration.environment).to be :production
+      end
+
+      it 'can set a login'  do
+        expect(Lce.configuration.login).to eq('login')
+      end
+      
+      it 'can set a password'  do
+        expect(Lce.configuration.password).to eq('password')
+      end
+    end      
+  end
 end
