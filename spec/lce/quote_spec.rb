300cbff1c33e731ccd69b753f7fd07613d764c9a
Florent <florent.piteau@sylogix.net>
2014-08-12 17:12:16 +0200

    Empty quote spec

--- spec/lce/quote_spec.rb
+++ spec/lce/quote_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
 describe Lce::Quote do
+  before do
+    Lce.configure do |config|
+      config.environment = :staging
+      config.login = 'login'
+      config.password = 'password'
+      config.logger.level = Logger::FATAL
+    end              
+  end
   describe ".request" do
-    it "requests a new quote" do
-      expect(Lce::Quote.request({})).to be({})
+    it "raises an error on an empty quote" do
+    
+      stub_request(:post, "https://login:password@test.lce.io/v1/quotes")
+        .with(:body => '{"quote":{}}')
+        .to_return(fixture('quotes/request/empty_quote'))
+        
+      expect{ Lce::Quote.request({})}.to raise_error(Lce::Client::Errors::LceError)
     end
+    
   end
 end
