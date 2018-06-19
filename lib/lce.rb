84a997aa5d81ab0dcd3fb0fc545d8d98213ec8e5
Thomas Belliard <thomas.belliard@sylogix.net>
2018-06-19 15:00:11 +0300

    Add v2 API support and use it by default.
    Bump to version 0.0.5.

--- lib/lce.rb
+++ lib/lce.rb
@@ -29,11 +29,11 @@ module Lce
   end
 
   def self.check
-    begin  
+    begin
       client.get
     rescue Lce::Client::Errors::LceError => e
       if configuration.raise_lce_errors
-        raise e      
+        raise e
       else
         configuration.logger.warn(e.to_s)
       end
@@ -47,20 +47,20 @@ module Lce
 
     def initialize
       @environment = :staging
-      @api_version = 1
+      @api_version = 2
       @http_adapter = Faraday.default_adapter
       @raise_lce_errors = true
       @logger = Logger.new(STDOUT)
-      @logger.level = Logger::DEBUG   
-      @application = 'ruby-lce'    
+      @logger.level = Logger::DEBUG
+      @application = 'ruby-lce'
       @version = Lce::VERSION
     end
-    
+
     def environment=(value)
       raise 'Environment must be :staging or :production' unless [:staging, :production].include?(value)
       @environment = value
     end
-    
+
     def application=(app)
       @application = "#{app} (ruby-lce)"
     end
@@ -70,4 +70,3 @@ module Lce
     end
   end
 end
-
--- lib/lce/client.rb
+++ lib/lce/client.rb
@@ -4,29 +4,29 @@ require 'lce/client/request'
 
 module Lce
   class Client
-  
+
     HOSTS = {
       development: "http://localhost:9000",
-      staging: "https://test.lce.io",
-      production: "https://api.lce.io"
+      staging: "https://test.myflyingbox.com",
+      production: "https://api.myflyingbox.com"
     }
-    
+
     include Connection
-    include Request  
+    include Request
     include Errors
-      
+
     attr_accessor :http_adapter
-    
+
     def initialize
       @http_adapter = Lce.configuration.http_adapter
     end
-    
+
     def host
       HOSTS[Lce.configuration.environment]
     end
-    
+
     def api_version
-      raise VersionError.new("Wrong API version",' wrong_api_version', "Version must be 1.") if Lce.configuration.api_version != 1
+      raise VersionError.new("Wrong API version",' wrong_api_version', "Version must be 1 or 2.") unless [1, 2].include?(Lce.configuration.api_version)
       'v'+Lce.configuration.api_version.to_s
     end
 
